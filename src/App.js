33707a4075645e2ad8d8608988dfabf50d8a4120
Atasha Potter <Ms.APotter@gmail.com>
2019-05-13 23:27:52 -0400

    small start added app css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Route, Switch, Link, BrowserRouter as Router} from 'react-router-dom';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super()
+
+
+  }
+
+  render() {
+    return (
+      <div className="body">
+      <h1>Hogwarts Houses</h1>
+        <nav>
+          <li><Link to="/gryffindor" className="nav">Gryffindor</Link></li>
+          <li><Link to="/ravenclaw" className="nav">Ravenclaw</Link></li>
+          <li><Link to="/griffindor" className="nav">Hufflepuff</Link></li>
+          <li><Link to="/slytherin" className="nav">Slytherin</Link></li>
+        </nav>
+
+        <div className="image-container">
+          <img src="https://images.pottermore.com/bxd3o8b291gf/4WGamkMvBmg2SE6g8y6IkY/07c89b29b0ec0287281f0cd16763a8f4/01-Godric-Small-GIF-2.gif" />
+          <img src="https://images.pottermore.com/bxd3o8b291gf/5opvBSIV8sEU2QyG8gSwMS/05fd83bda2b6746751018095a9061d0f/03-Ravenclaw-Small-GIF-1.gif?w=1100&q=85" />
+          <img src="https://images.pottermore.com/bxd3o8b291gf/3afkxANXQAW0G8uOgwYYYU/339684d4be8818c4a7ef639d5bbfe7cb/04-Helga-Small-GIF-1.gif?w=1100&q=85" />
+          <img src ="https://images.pottermore.com/bxd3o8b291gf/x6JKzQ2eC4iyQaGeQI4AO/fdb195732fccab5f68bd7ddf3c7c2421/02-Salazar-Small-GIF-1.gif?w=1100&q=85" />
+        </div>
+
+        <Switch>
+          {/* <Route
+            exact path="/"
+            render={routerProps => (
+              <Home {...routerProps} {...this.state} />
+            )}
+          />
+          <Route
+            exact path="/gryffindor"
+            render={routerProps => (
+              <Show {...routerProps} {...this.state} />
+            )}
+          /> */}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
