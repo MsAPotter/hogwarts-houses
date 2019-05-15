e4386fc9455c1db7f6babe4d517f35904b51adc5
Atasha Potter <Ms.APotter@gmail.com>
2019-05-15 12:00:00 -0400

    works but pulls data from api directly and not localhost aka frontend and backend arent talking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Home from './Home/Home';
 import Gryffindor from './Gryffindor/Gryffindor';
 import Hufflepuff from './Hufflepuff/Hufflepuff';
 import Ravenclaw from './Ravenclaw/Ravenclaw';
 import Slytherin from './Slytherin/Slytherin';
-import characters from './characters.json';
+// import characters from './characters.json';
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
     super()
 
     this.state = {
-      characters: characters
+      // characters: characters   ==> if manually pulling from hard-coded json
+      characters: []
     }
   }
 
+  componentDidMount () {
+    axios.get('https://www.potterapi.com/v1/characters/?key=$2a$10$63GTbZLWfMtyBrCfDRaRyus98dC5tg6PikwPTPaVUgtJkUeEBsJpy')
+    // axios.get('localhost:4000/hp/')
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          characters: res.data
+        })
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   render() {
-    console.log(this.state)
     return (
       <div>
         <Switch>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Home.css';
 
 class Home extends Component {
@@ -12,22 +12,22 @@ class Home extends Component {
             <div className="container">
                 <div>
                 <Link to="/gryffindor" className="nav"><li>Gryffindor</li>
-                <img src="https://images.pottermore.com/bxd3o8b291gf/4WGamkMvBmg2SE6g8y6IkY/07c89b29b0ec0287281f0cd16763a8f4/01-Godric-Small-GIF-2.gif" />
+                <img src="https://images.pottermore.com/bxd3o8b291gf/4WGamkMvBmg2SE6g8y6IkY/07c89b29b0ec0287281f0cd16763a8f4/01-Godric-Small-GIF-2.gif" alt={"Gryffindor founder"}/>
                 </Link>
                 </div>
                 <div>
                 <Link to="/ravenclaw" className="nav"><li>Ravenclaw</li>
-                <img src="https://images.pottermore.com/bxd3o8b291gf/5opvBSIV8sEU2QyG8gSwMS/05fd83bda2b6746751018095a9061d0f/03-Ravenclaw-Small-GIF-1.gif?w=1100&q=85" />
+                <img src="https://images.pottermore.com/bxd3o8b291gf/5opvBSIV8sEU2QyG8gSwMS/05fd83bda2b6746751018095a9061d0f/03-Ravenclaw-Small-GIF-1.gif?w=1100&q=85" alt={"Ravenclaw founder"}/>
                 </Link>
                 </div>
                 <div>
                 <Link to="/hufflepuff" className="nav"><li>Hufflepuff</li>
-                <img src="https://images.pottermore.com/bxd3o8b291gf/3afkxANXQAW0G8uOgwYYYU/339684d4be8818c4a7ef639d5bbfe7cb/04-Helga-Small-GIF-1.gif?w=1100&q=85" />
+                <img src="https://images.pottermore.com/bxd3o8b291gf/3afkxANXQAW0G8uOgwYYYU/339684d4be8818c4a7ef639d5bbfe7cb/04-Helga-Small-GIF-1.gif?w=1100&q=85" alt={"Hufflepuff founder"}/>
                 </Link>
                 </div>
                 <div>
                 <Link to="/slytherin" className="nav"><li>Slytherin</li>
-                <img src ="https://images.pottermore.com/bxd3o8b291gf/x6JKzQ2eC4iyQaGeQI4AO/fdb195732fccab5f68bd7ddf3c7c2421/02-Salazar-Small-GIF-1.gif?w=1100&q=85" />
+                <img src ="https://images.pottermore.com/bxd3o8b291gf/x6JKzQ2eC4iyQaGeQI4AO/fdb195732fccab5f68bd7ddf3c7c2421/02-Salazar-Small-GIF-1.gif?w=1100&q=85" alt={"Slytherin founder"}/>
                 </Link>
                 </div>
             </div>
